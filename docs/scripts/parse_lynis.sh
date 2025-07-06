55c2870f8db3c079c1808b47490b6c29e2bf9e3a
rojounooo <rojounooo@gmail.com>
2025-07-06 11:35:17 +0100

    Updated lynis parsing script

--- docs/scripts/parse_lynis.sh
+++ docs/scripts/parse_lynis.sh
@@ -1,20 +1,22 @@
 #!/bin/bash
 
 # Variables
-LYNIS_PATH=$1
-OUTPUT_PATH=$2
+LYNIS_PATH=/var/log/lynis-report.dat # Path to the Lynis report file
+OUTPUT_PATH=/home/admin/parsed_lynis_report.txt # Path to the output file
 
-# Get hardening index score 
-echo "Hardening Index:" > $OUTPUT_PATH
-echo "----------------" >> $OUTPUT_PATH
-echo $(grep "index" $LYNIS_PATH | cut -d: -f2) >> $OUTPUT_PATH
+# Get hardening index score
+echo "Hardening Index:" > "$OUTPUT_PATH"
+echo "----------------" >> "$OUTPUT_PATH"
+grep "hardening_index=" "$LYNIS_PATH" | cut -d'=' -f2 >> "$OUTPUT_PATH"
+echo "" >> "$OUTPUT_PATH"
 
-# Get warnings 
-echo "Warnings:" >> $OUTPUT_PATH
-echo "----------------" >> $OUTPUT_PATH
-grep "warning[]" $LYNIS_PATH | cut -d: -f2 >> $OUTPUT_PATH
+# Get warnings
+echo "Warnings:" >> "$OUTPUT_PATH"
+echo "----------------" >> "$OUTPUT_PATH"
+grep "^warning\[\]=" "$LYNIS_PATH" | cut -d'=' -f2 >> "$OUTPUT_PATH"
+echo "" >> "$OUTPUT_PATH"
 
 # Get suggestions
-echo "Suggestions:" >> $OUTPUT_PATH
-echo "----------------" >> $OUTPUT_PATH
-grep "suggestion[]" $LYNIS_PATH | cut -d: -f2 >> $OUTPUT_PATH
+echo "Suggestions:" >> "$OUTPUT_PATH"
+echo "----------------" >> "$OUTPUT_PATH"
+grep "^suggestion\[\]=" "$LYNIS_PATH" | cut -d'=' -f2 >> "$OUTPUT_PATH"
