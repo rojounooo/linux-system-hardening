fce0415454d61c629581e5839596379db2068861
rojounooo <rojounooo@gmail.com>
2025-07-29 15:25:20 +0100

    Created script to automate patch management

---
+++ scripts/bash/patch and packages/setup_management.sh
@@ -0,0 +1,26 @@
+#!/bin/bash 
+
+# === debsums implementation ===
+
+echo "Installing debsums..."    
+sudo apt install debsums -y
+
+echo "Verifying installed packages..."
+sudo debsums -y
+
+# === Patch Management Visibility ===
+
+echo "Installing show-versions"
+sudo apt install apt-show-versions -y
+
+echo "Displaying available package versions"
+apt-show-versions | grep upgradeable
+
+# === Monitoring bugs and changes ===
+
+echo "Installing listbugs and listchanges"
+sudo apt install apt-listbugs apt-listchanges -y
+
+# === Removing unused packages ===
+echo "Removing unused packages..."
+sudo apt autoremove -y
